5b8a897a8b76188378b64861d9b836536e8c3229
pierre pracht <pierre.pracht@gmail.com>
2023-04-10 16:44:08 +0200

    first commit

---
+++ src/components/Card.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps({ id: String, value: String })
+</script>
+
+<template>
+  <div>
+    <h3>Card</h3>
+    <p>id: {{ id }}</p>
+    <p>value: {{ value }}</p>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import PocketBase, { Record } from 'pocketbase'
+import Card from '@/components/Card.vue'
+
+const events: Record[] = await new Promise((resolve) => {
+  setTimeout(
+    () =>
+      resolve([
+        new Record({ id: 1, value: '11' }),
+        new Record({ id: 2, value: '22' }),
+        new Record({ id: 3, value: '33' })
+      ]),
+    500
+  )
+})
+</script>
+
+<template>
+  <main>
+    <card v-for="event in events" v-bind="event" :key="event.id" />
+  </main>
+</template>
