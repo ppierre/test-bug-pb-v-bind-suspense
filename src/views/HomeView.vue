5f6ffc03e6134fa1d9ab4f5841a2edfbe4e6f661
pierre pracht <pierre.pracht@gmail.com>
2023-04-10 17:15:36 +0200

    Revert "ok avec dummy Record object."
    This reverts commit f5ac0b14204df07c0fcc0018215253d545a6d40a.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,28 +1,12 @@
 <script setup lang="ts">
-//import PocketBase, { Record } from 'pocketbase'
+import PocketBase, { Record } from 'pocketbase'
 import Card from '@/components/Card.vue'
-
-class Record {
-  id: number
-  value: string
-
-  constructor(obj: { id: number; value: string }) {
-    this.id = obj.id
-    this.value = obj.value
-  }
-
-  $load(value: string) {
-    this.value = value
-    console.log(value)
-  }
-}
 
 const events: Record[] = [
   new Record({ id: 1, value: '11' }),
   new Record({ id: 2, value: '22' }),
   new Record({ id: 3, value: '33' })
 ]
-debugger
 </script>
 
 <template>
